b8e4aeafb6b6a348ac4559bc67cea1b14b6bcca6
Andres Calles <meridaalejandro81@gmail.com>
2026-01-20 03:05:08 -0400

    Arreglo de bugs

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import app from "./src/app.js";
-const PORT = process.env.PORT || 3000;
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
