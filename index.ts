ca46b822ef8f15b4227509b1e0f6cf2a09d6a71b
Andres Calles <meridaalejandro81@gmail.com>
2026-01-28 19:39:42 -0400

    Añadido cors

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
+app.use(cors());
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
+
+// Código para obtener las versiones de geminis disponibles
 // async function listModels() {
 //   try {
 //     const response = await fetch(`https://generativelanguage.googleapis.com/v1/models?key=${process.env.GEMINI_API_KEY}`);
