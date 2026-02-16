0a7f59c8f28c25bf69e41b1e54cbf772c2e0248c
Andres Calles <meridaalejandro81@gmail.com>
2026-02-16 12:11:19 -0400

    Query sql mejorada para adminSuperior

--- src/models/AdminSuperior.ts
+++ src/models/AdminSuperior.ts
@@ -2,43 +2,46 @@ import { query } from "../connect/connect.js";
 import type { GymSummary, PlanUpgradeData } from "../types/AdminSuperior.js";
 
 export const getAllGymsData = async (): Promise<GymSummary[]> => {
-    const sql = `
-        SELECT 
-            g.id, 
-            g.name_gym, 
-            g.system_plan, 
-            u.name as owner_name, 
-            u.email as owner_email,
-            COALESCE(s.status, 'inactive') as status,
-            MAX(s.end_date) as expiration_date
-        FROM gyms g
-        INNER JOIN users u ON g.id = u.gym_id
-        LEFT JOIN gym_subscriptions s ON g.id = s.gym_id
-        WHERE u.role = 'gym_owner'
-        GROUP BY g.id, u.name, u.email, s.status;
+  const sql = `
+        SELECT DISTINCT ON (g.id)
+    g.id,
+    g.name_gym,
+    u.name as owner_name,
+    u.email as owner_email,
+    s.plan_type,
+    s.status,
+    s.end_date as expiration_date
+FROM gyms g
+INNER JOIN users u ON g.id = u.gym_id
+LEFT JOIN gym_subscriptions s ON g.id = s.gym_id
+WHERE u.role = 'gym_owner'
+ORDER BY g.id, s.end_date DESC;
+
     `;
-    const result = await query(sql);
-    return result.rows;
+  const result = await query(sql);
+  return result.rows;
 };
 
-export const updatePlanGyms = async (data: PlanUpgradeData)=> {
-    await query("BEGIN");
-    try {
-        const { gymId, planType, price, monts} = data;
-        await query("UPDATE gyms SET system_plan = $1 WHERE id = $2", [planType, gymId]);
+export const updatePlanGyms = async (data: PlanUpgradeData) => {
+  await query("BEGIN");
+  try {
+    const { gymId, planType, price, monts } = data;
+    await query("UPDATE gyms SET system_plan = $1 WHERE id = $2", [
+      planType,
+      gymId,
+    ]);
 
-        const endDate = new Date();
-        endDate.setMonth(endDate.getMonth() + monts);
+    const endDate = new Date();
+    endDate.setMonth(endDate.getMonth() + monts);
 
-        await query(
-            "INSERT INTO gym_subscriptions (gym_id, plan_type, price_paid, end_date) VALUES ($1, $2, $3, $4)",
-            [gymId, planType, price, endDate]
-        )
+    await query(
+      "INSERT INTO gym_subscriptions (gym_id, plan_type, price_paid, end_date) VALUES ($1, $2, $3, $4)",
+      [gymId, planType, price, endDate],
+    );
 
-        await query("COMMIT");
-    }catch(error){
-        await query("ROLLBACK");
-        throw error;
-    
-    }
-}
+    await query("COMMIT");
+  } catch (error) {
+    await query("ROLLBACK");
+    throw error;
+  }
+};
