8c78626e9c7ad886c8bd28b168f41bcd1649f65e
Andres Calles <meridaalejandro81@gmail.com>
2026-03-03 14:25:52 -0400

    Corregido fecha a renovar membresias 7

--- src/models/Memberships.ts
+++ src/models/Memberships.ts
@@ -30,7 +30,7 @@ export const registerMembership = async ( data: CreateMembershipDTO): Promise<Me
 
 // Funcion para obtener la membresía por gym_id
 export const getMembershipByGymId = async (gym_id: number): Promise<MembershipBody[]> => {
-  const sql = ` SELECT  m.id, m.plan_id, c.name as client_name, c.phone as client_phone, p.name as plan_name,  p.price as plan_price,m.fecha_inicio,
+  const sql = ` SELECT  m.id, m.client_id, m.plan_id, c.name as client_name, c.phone as client_phone, p.name as plan_name,  p.price as plan_price,m.fecha_inicio,
       m.fecha_membresias as fecha_vencimiento, CASE WHEN m.fecha_membresias <= CURRENT_DATE THEN 'vencido' ELSE 'activo'
       END AS estado FROM memberships m JOIN clients c ON m.client_id = c.id  JOIN plans p ON m.plan_id = p.id 
        WHERE m.gym_id = $1  ORDER BY m.fecha_membresias ASC`;
@@ -40,7 +40,7 @@ export const getMembershipByGymId = async (gym_id: number): Promise<MembershipBo
 
 // Funcion para obtener la membresía por id
 export const getMembershipById = async (id: number, gym_id: number): Promise<MembershipBody | null> => {
-    const sql = `SELECT m.id, m.plan_id, c.name as client_name, c.phone as client_phone, p.name as plan_name,  p.price as plan_price,m.fecha_inicio,
+    const sql = `SELECT m.id, m.gym_id, m.client_id, m.plan_id, c.name as client_name, c.phone as client_phone, p.name as plan_name,  p.price as plan_price,m.fecha_inicio,
       m.fecha_membresias as fecha_vencimiento, CASE WHEN m.fecha_membresias <= CURRENT_DATE THEN 'vencido' ELSE 'activo'
       END AS estado FROM memberships m JOIN clients c ON m.client_id = c.id  JOIN plans p ON m.plan_id = p.id WHERE m.id = $1 AND m.gym_id = $2`;
     const result = await query(sql, [id, gym_id]);
