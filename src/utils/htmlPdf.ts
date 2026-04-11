7b59dfac910abf7be7446cb2cd61c693b0043787
Andres Calles <meridaalejandro81@gmail.com>
2026-04-10 22:37:10 -0400

    Url del carnet arreglado

--- src/utils/htmlPdf.ts
+++ src/utils/htmlPdf.ts
@@ -5,7 +5,8 @@ import * as html_pdf from 'html-pdf-node';
 export async function generarCarnetBuffer(datos: {gymName: string, userName: string, cedula: string, phone: string, planName: string, idMembresia: number}): Promise<Buffer> {
     
     // 1. URL de validación para el QR
-    const urlVerificacion = `https://frontend-gym-topaz.vercel.app/verify/${datos.idMembresia}`;
+    const frontendUrl = `https://frontend-gym-topaz.vercel.app`
+    const urlVerificacion = `${frontendUrl}/verify/${datos.idMembresia}`;
     const qrBase64 = await QRCode.toDataURL(urlVerificacion);
 
     // 2. HTML del Carnet (Inline para rapidez, puedes usar un template aparte)
