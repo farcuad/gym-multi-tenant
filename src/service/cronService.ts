8e6f81b23e94b7949c07d2ce845f85a13f760989
Andres Calles <meridaalejandro81@gmail.com>
2026-05-14 14:49:10 -0400

    Corregido mensajes de recordatorio

--- src/service/cronService.ts
+++ src/service/cronService.ts
@@ -16,6 +16,7 @@ interface Clients {
     telefono: string;
     fecha_membresia: string;
     gym_nombre: string;
+    plan_nombre: string;
     dias_para_vencer: number;
     whaibot_id: string | null;
     whaibot_key: string | null;
@@ -74,6 +75,7 @@ export const ejecutarNotifiaciones = async () => {
                 c.phone AS telefono, 
                 m.fecha_membresias AS fecha_membresia, 
                 g.name_gym AS gym_nombre,
+                m.plan_name_purchase AS plan_nombre,
                 (m.fecha_membresias::date - $1::date) AS dias_para_vencer,
                 gbc.whaibot_id,
                 gbc.whaibot_key
@@ -109,9 +111,9 @@ export const ejecutarNotifiaciones = async () => {
 
                 let mensaje = "";
                  if (cliente.dias_para_vencer === 0) {
-                     mensaje = `⏰ *¡VENCE HOY!* ⏰\n\nHola *${cliente.cliente_nombre}*, tu membresía en *${cliente.gym_nombre}* vence hoy *${fechaLimpia}*. ¡Te esperamos para renovar y seguir con tus metas! 💪🔥`;
+                     mensaje = `⏰ *¡VENCE HOY!* ⏰\n\nHola *${cliente.cliente_nombre}*, tu membresía de *${cliente.plan_nombre}* en *${cliente.gym_nombre}* vence hoy *${fechaLimpia}*. ¡Te esperamos para renovar y seguir con tus metas! 💪🔥`;
                     } else {
-                    mensaje = `🔔 *RECORDATORIO* 🔔\n\nHola *${cliente.cliente_nombre}*, te recordamos que tu membresía en *${cliente.gym_nombre}* vencerá en *3 días* *${fechaLimpia}*. ¡Anticípate y mantente activo! 🏋️‍♂️✨`;
+                    mensaje = `🔔 *RECORDATORIO* 🔔\n\nHola *${cliente.cliente_nombre}*, te recordamos que tu membresía de *${cliente.plan_nombre}* en *${cliente.gym_nombre}* vencerá en *3 días* *${fechaLimpia}*. ¡Anticípate y mantente activo! 🏋️‍♂️✨`;
                 }
 
             try {
