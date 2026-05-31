1b66f90735af751eb3e3318336324b2c8dd1a734
Andres Calles <meridaalejandro81@gmail.com>
2026-05-30 23:05:06 -0400

    Error de cors arreglado

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -303,7 +303,7 @@ export const loginClient = async (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
